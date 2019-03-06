c45ff07660ea7c3597f58baf29e8865e68bdb16c
649248118@qq.com <649248118@qq.com>
2019-03-06 11:33:11 +0800

    add test project

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using Common.UniqueIdGenerator;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueGenerator ug = new UniqueGenerator(1, 1, 1);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(ug.GetNextId());
+            }
+
+            Console.WriteLine("end..");
+            Console.Read();
+        }
+    }
+}
